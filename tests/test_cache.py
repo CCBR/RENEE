60ac60703ad13fcb5d0d63ab5a6fc985b29ebec5
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-02-16 10:29:11 -0500

    test: image caching behavior with & without --sif-cache

---
+++ tests/test_cache.py
@@ -0,0 +1,45 @@
+import tempfile
+import json
+import os.path
+import subprocess
+
+renee_run = (
+    "src/renee/__main__.py run "
+    "--mode local --runmode init --dry-run "
+    "--input .tests/*.fastq.gz "
+    "--genome hg38_30 "
+)
+
+
+def run_in_temp(command_str):
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        outdir = os.path.join(tmp_dir, "testout")
+        output = subprocess.run(
+            f"{command_str} --output {outdir}",
+            capture_output=True,
+            shell=True,
+            text=True,
+        )
+        with open(os.path.join(outdir, "config.json"), "r") as infile:
+            config = json.load(infile)
+    return output, config
+
+
+def test_cache_sif():
+    output, config = run_in_temp(f"{renee_run} --sif-cache tests/data/sifs/")
+    assertions = [
+        config["images"]["arriba"].endswith(
+            "tests/data/sifs/ccbr_arriba_2.0.0_v0.0.1.sif"
+        ),
+        "does not exist in singularity cache" in output.stderr,
+    ]
+    assert all(assertions)
+
+
+def test_cache_nosif():
+    output, config = run_in_temp(f"{renee_run}")
+    assertions = [
+        config["images"]["arriba"] == "docker://nciccbr/ccbr_arriba_2.0.0:v0.0.1",
+        "The singularity command has to be available" in output.stderr,
+    ]
+    assert all(assertions)
