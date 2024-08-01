70ee7ad1b4e9151998445a3e9245d9a061996148
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-08-01 17:15:14 -0400

    feat: set hg38_36 as the default genome
    resolves #128

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,12 +1,31 @@
 import pytest
 import subprocess
+import tempfile
+import json
+import os.path
 
 renee_run = (
     "src/renee/__main__.py run "
     "--mode local --runmode init --dry-run "
     "--input .tests/*.fastq.gz "
-    "--genome config/genomes/biowulf/hg38_30.json "
 )
+
+
+def run_in_temp(command_str):
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        outdir = os.path.join(tmp_dir, "testout")
+        output = subprocess.run(
+            f"{command_str} --output {outdir}",
+            capture_output=True,
+            shell=True,
+            text=True,
+        )
+        if os.path.exists(os.path.join(outdir, "config.json")):
+            with open(os.path.join(outdir, "config.json"), "r") as infile:
+                config = json.load(infile)
+        else:
+            config = None
+    return output, config
 
 
 def test_help():
@@ -43,5 +62,25 @@ def test_subcommands_help():
                 text=True,
             ).stdout
             for cmd in ["run", "build", "cache", "unlock"]
+        ]
+    )
+
+
+def test_default_genome():
+    output, config = run_in_temp(renee_run)
+    assert all(
+        [
+            "hg38_36" in config["references"]["rnaseq"]["GENOME"],
+            "The singularity command has to be available" in output.stderr,
+        ]
+    )
+
+
+def test_genome_param():
+    output, config = run_in_temp(f"{renee_run} --genome hg19_19")
+    assert all(
+        [
+            "hg19_19" in config["references"]["rnaseq"]["GENOME"],
+            "The singularity command has to be available" in output.stderr,
         ]
     )
