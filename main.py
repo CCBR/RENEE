f6f751b2ddd69877a13971bfdf493795302d7e99
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-08-05 15:36:20 -0400

    chore: remove reference to champagne

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import re
 import sys
 from src.renee.__main__ import main
 
-# add script directory to the path to allow champagne CLI to work out-of-the-box
+# add script directory to the path to allow the CLI to work out-of-the-box
 # without the need to install it via pip first
 SCRIPT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "src", "renee")
 sys.path.append(SCRIPT_DIR)
